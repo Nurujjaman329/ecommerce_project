956ddaded468f219f84600c33352f519c702127a
Nurujjaman329 <mdnurujjaman329@gmail.com>
2024-03-09 22:52:53 +0600

    add bottomNavigationbar screens

--- lib/views/buyers/main_screens.dart
+++ lib/views/buyers/main_screens.dart
@@ -1,3 +1,9 @@
+import 'package:ecommerce_practise/views/buyers/nav_screens/account_screen.dart';
+import 'package:ecommerce_practise/views/buyers/nav_screens/cart_screen.dart';
+import 'package:ecommerce_practise/views/buyers/nav_screens/category_screen.dart';
+import 'package:ecommerce_practise/views/buyers/nav_screens/home_screens.dart';
+import 'package:ecommerce_practise/views/buyers/nav_screens/search_screen.dart';
+import 'package:ecommerce_practise/views/buyers/nav_screens/store_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,44 +15,62 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  List<Widget> _pages = [
+    HomeScreen(),
+    CategoryScreen(),
+    StoreScreen(),
+    CartScreen(),
+    SearchScreen(),
+    AccountScreen(),
+  ];
+
+  int _pageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-          unselectedItemColor: Colors.black,
-          selectedItemColor: Colors.yellow.shade900,
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(
-                  CupertinoIcons.home,
-                ),
-                label: 'HOME'),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.category_outlined,
-                ),
-                label: 'Categories'),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.store,
-                ),
-                label: 'Store'),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  CupertinoIcons.cart,
-                ),
-                label: 'Cart'),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  CupertinoIcons.search,
-                ),
-                label: 'Search'),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  CupertinoIcons.profile_circled,
-                ),
-                label: 'Account'),
-          ]),
+        currentIndex: _pageIndex,
+        onTap: (value) {
+          setState(() {
+            _pageIndex = value;
+          });
+        },
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.yellow.shade900,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                CupertinoIcons.home,
+              ),
+              label: 'HOME'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.category_outlined,
+              ),
+              label: 'Categories'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.store,
+              ),
+              label: 'Store'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                CupertinoIcons.cart,
+              ),
+              label: 'Cart'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                CupertinoIcons.search,
+              ),
+              label: 'Search'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                CupertinoIcons.profile_circled,
+              ),
+              label: 'Account'),
+        ],
+      ),
+      body: _pages[_pageIndex],
     );
   }
 }
