79408578237fcafe86949d0dddfe767159196f98
Nurujjaman329 <mdnurujjaman329@gmail.com>
2024-03-17 22:28:44 +0600

    banner add

--- lib/views/buyers/nav_screens/widgets/banner_widget.dart
+++ lib/views/buyers/nav_screens/widgets/banner_widget.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class BannerWidget extends StatefulWidget {
@@ -8,21 +9,46 @@ class BannerWidget extends StatefulWidget {
 }
 
 class _BannerWidgetState extends State<BannerWidget> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final List _bannerImage = [];
+
+  getBanners() {
+    return _firestore
+        .collection('banners')
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        setState(() {
+          _bannerImage.add(doc['image']);
+        });
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    getBanners();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 140,
-      width: MediaQuery.of(context).size.width - 40,
-      decoration: BoxDecoration(
-          color: Colors.yellow.shade900,
-          borderRadius: BorderRadius.circular(10.0)),
-      child: PageView(
-        children: [
-          Center(child: Text('Banner 1')),
-          Center(child: Text('Banner 2')),
-          Center(child: Text('Banner 3')),
-        ],
-      ),
-    );
+        height: 140,
+        width: MediaQuery.of(context).size.width - 40,
+        decoration: BoxDecoration(
+            color: Colors.yellow.shade900,
+            borderRadius: BorderRadius.circular(10.0)),
+        child: PageView.builder(
+            itemCount: _bannerImage.length,
+            itemBuilder: (context, index) {
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: Image.network(
+                  _bannerImage[index],
+                  fit: BoxFit.cover,
+                ),
+              );
+            }));
   }
 }
