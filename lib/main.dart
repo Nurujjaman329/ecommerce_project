e0593aac25837cb95711574016e3e3c91b776e45
Nurujjaman329 <mdnurujjaman329@gmail.com>
2024-03-17 16:22:13 +0600

    initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
+import 'dart:io';
+
 import 'package:ecommerce_practise/views/buyers/main_screens.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: FirebaseOptions(
+              apiKey: "AIzaSyAHMzcxoczO33PEUny9SnUHHaVplD1osEU",
+              appId: "1:479653429197:android:b98624f494c2a59ba18e84",
+              messagingSenderId: "479653429197",
+              projectId: "ecommerce-17-3-24",
+              storageBucket: "gs://ecommerce-17-3-24.appspot.com"))
+      : await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
